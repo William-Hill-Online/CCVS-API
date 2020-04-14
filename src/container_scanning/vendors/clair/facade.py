ea1d5794306ae3d70f8f69013df2825f1270dc30
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2020-04-15 01:34:05 +0200

    feature: add function return vulnabilities by severity

--- src/container_scanning/vendors/clair/facade.py
+++ src/container_scanning/vendors/clair/facade.py
@@ -44,3 +44,27 @@ def get_vuln(config, image_id):
         return obj
     finally:
         fp.close()
+
+
+def get_resume(result):
+
+    resume = {}
+
+    ancestry = result.get('ancestry')
+    for layer in ancestry.get('layers'):
+        detected_features = layer.get('detected_features')
+        for detected_feature in detected_features:
+            for vulnerability in detected_feature.get('vulnerabilities'):
+                severity = vulnerability.get('severity')
+
+                if severity in ['Critical', 'Defcon1']:
+                    key = 'critical_vulns'
+                else:
+                    # Unknown, Negligible, Low, Medium, High
+                    key = severity.lower() + '_vulns'
+
+                if not resume.get(key):
+                    resume[key] = []
+                resume[key].append(vulnerability)
+
+    return resume
