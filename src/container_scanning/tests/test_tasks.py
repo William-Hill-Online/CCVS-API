876a162d25af82021d41a06d74370058696375ab
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2020-04-15 01:41:35 +0200

    feature: change tasks to work with analysis instead job

--- src/container_scanning/tests/test_tasks.py
+++ src/container_scanning/tests/test_tasks.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch
 
-from container_scanning.models import Job, Vendor
+from container_scanning.models import Analysis, Vendor
 from container_scanning.tasks import scan_image, scan_image_vendor, scan_image_vendors
 from django.test.testcases import TestCase
 
@@ -10,22 +10,37 @@ class TasksTest(TestCase):
     def test_scan_image_success(self, scan_image_vendors):
         """Ensure we can create a scan."""
         scan_image_vendors.return_value = [
-            ('anchore', {'result': 'anchore_result'}),
-            ('clair', {'result': 'clair_result'}),
+            (
+                'anchore',
+                {'result': 'anchore_result'},
+                {'low': [{'key': 'value'}]}
+            ), (
+                'clair',
+                {'result': 'clair_result'},
+                {}
+            ),
         ]
-        job = Job.objects.create(data={'image': 'image_tag'})
+        analysis = Analysis.objects.create(image='image_tag')
 
-        scan_image(job_id=job.id, data={'image': 'image_tag'})
+        scan_image(analysis_id=analysis.id, image='image_tag')
 
-        job_finished = Job.objects.get(id=job.id)
+        analysis_finished = Analysis.objects.get(id=analysis.id)
         scan_image_vendors.assert_called_with('image_tag')
         self.assertDictEqual(
-            job_finished.result,
+            analysis_finished.vendors,
             {
                 'clair': {'result': 'clair_result'},
                 'anchore': {'result': 'anchore_result'},
             },
         )
+        self.assertDictEqual(
+            analysis_finished.vulnerabilities,
+            {
+                'clair': {},
+                'anchore': {'low': [{'key': 'value'}]},
+            },
+        )
+        self.assertEqual(analysis_finished.result, 'failed')
 
     @patch('container_scanning.tasks.initialize')
     def test_scan_image_vendor_success(self, initialize):
@@ -40,17 +55,21 @@ class TasksTest(TestCase):
 
         initialize.add_image.return_value = 'image_id'
         initialize.get_vuln.return_value = {'key': 'value'}
+        initialize.get_resume.return_value = {'key2': 'value2'}
 
         result = scan_image_vendor('image_tag', vendor)
 
         initialize.add_image.assert_called_with(
-            {'user': 'user', 'pass': 'password'}, tag='image_tag'
+            {'user': 'user', 'pass': 'password'},
+            tag='image_tag'
         )
-        initialize.get_vuln.assert_called_with(
-            {'user': 'user', 'pass': 'password'}, image_id='image_id'
+        initialize.get_vuln.assert_called_with({
+            'user': 'user', 'pass': 'password'
+        }, image_id='image_id'
         )
+        initialize.get_resume.assert_called_with({'key': 'value'})
 
-        self.assertDictEqual(result, {'key': 'value'})
+        self.assertTupleEqual(result, ({'key': 'value'}, {'key2': 'value2'}))
 
     @patch('container_scanning.tasks.scan_image_vendor')
     def test_scan_image_vendors_success(self, scan_image_vendor):
@@ -70,16 +89,21 @@ class TasksTest(TestCase):
             }
         )
 
-        scan_image_vendor.side_effect = ['result1', 'result2']
+        scan_image_vendor.side_effect = [
+            ({'key': 'value'}, {'key2': 'value2'}),
+            ({'key3': 'value3'}, {'key4': 'value4'})
+        ]
 
         results = scan_image_vendors('image_tag')
 
         result = next(results)
         scan_image_vendor.assert_called_with('image_tag', vendor1)
         self.assertEqual(result[0], 'VendorExample1')
-        self.assertEqual(result[1], 'result1')
+        self.assertDictEqual(result[1], {'key': 'value'})
+        self.assertDictEqual(result[2], {'key2': 'value2'})
 
         result = next(results)
         scan_image_vendor.assert_called_with('image_tag', vendor2)
         self.assertEqual(result[0], 'VendorExample2')
-        self.assertEqual(result[1], 'result2')
+        self.assertDictEqual(result[1], {'key3': 'value3'})
+        self.assertDictEqual(result[2], {'key4': 'value4'})
