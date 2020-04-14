503a546e35991b1dedda8c24e79ed3d970e930e9
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2020-04-14 15:57:35 +0200

    feature: add analysis model

--- src/container_scanning/models.py
+++ src/container_scanning/models.py
@@ -37,3 +37,35 @@ class Job(models.Model):
             from .tasks import scan_image
 
             scan_image.delay(job_id=self.id, data=self.data)
+
+
+class Analysis(models.Model):
+
+    # list of statuses that analysis can have
+    STATUSES = (
+        ('pending', 'pending'),
+        ('started', 'started'),
+        ('finished', 'finished'),
+        ('failed', 'failed'),
+    )
+    RESULTS = (
+        ('pending', 'pending'),
+        ('passed', 'passed'),
+        ('failed', 'failed'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    status = models.CharField(choices=STATUSES, max_length=20, default=STATUSES[0][0])
+    result = models.CharField(choices=RESULTS, max_length=20, default=RESULTS[0][0])
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    vulnerabilities = JSONField(null=True)
+    vendors = JSONField(null=True)
+
+    def save(self, *args, **kwargs):
+        super(Analysis, self).save(*args, **kwargs)
+        if self.status == 'pending':
+            from .tasks import scan_image
+
+            scan_image.delay(job_id=self.id, data=self.data)
