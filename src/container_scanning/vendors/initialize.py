6881bbef493d927f5ef19bd23110962669ad5800
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2020-05-05 03:08:37 +0200

    feature(vendors): add exceptions in initialize

--- src/container_scanning/vendors/initialize.py
+++ src/container_scanning/vendors/initialize.py
@@ -1,9 +1,20 @@
 import importlib
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def initialize(name):
     try:
-        module = importlib.import_module(f'container_scanning.vendors.{name}.facade')
+        module = importlib.import_module(
+            f'container_scanning.vendors.{name}.facade')
+    except Exception as error:
+        msg_error = f'Error initializing vendor {name}',
+        logger.exception(msg={'error': msg_error, 'exception': error})
+        raise Exception(msg_error)
+    else:
+        if not module:
+            msg_error = f'Vendor {name} not initialized'
+            logger.exception(msg={'error': msg_error})
+            raise Exception(msg_error)
         return module
-    except Exception:
-        return False
